e3e8eb64edc2abfdc4c711c8cadb08ca7d037062
birdpump <birdpump@gmail.com>
2024-10-31 09:04:02 -0700

    add transition

--- src/lib/components/VideoItem.svelte
+++ src/lib/components/VideoItem.svelte
@@ -1,18 +1,27 @@
 <script>
-    export let videoUrl = ''; // Change from imageUrl to videoUrl
+    import {onMount} from 'svelte';
+    import {scale} from 'svelte/transition';
+
+    export let videoUrl = '';
     export let text = '';
+
+    let visible = false;
+
+    onMount(() => {
+        visible = true;
+    });
 </script>
 
 <style>
     .carousel-element {
-        height: calc(100vh - 60px); /* Adjust as needed */
+        height: calc(100vh - 60px);
         overflow: hidden;
-        position: relative; /* Make this relative for absolute positioning of video */
+        position: relative;
 
         display: flex;
         align-items: center;
         justify-content: center;
-        overflow: hidden; /* Ensure overflow is hidden for the video */
+        overflow: hidden;
     }
 
     .carousel-text {
@@ -21,8 +30,8 @@
         text-align: center;
         color: white;
         text-shadow: 5px 5px 10px rgba(0, 0, 0, 0.8);
-        position: absolute; /* Position text over the video */
-        z-index: 1; /* Ensure text is above the video */
+        position: absolute;
+        z-index: 1;
     }
 
     @media only screen and (max-width: 600px) {
@@ -36,22 +45,24 @@
     }
 
     video {
-        position: absolute; /* Position video absolutely to fill the element */
+        position: absolute;
         top: 50%;
         left: 50%;
-        min-width: 100%; /* Ensure the video fills the width */
-        min-height: 100%; /* Ensure the video fills the height */
-        width: auto; /* Allow width to auto adjust */
-        height: auto; /* Allow height to auto adjust */
-        transform: translate(-50%, -50%); /* Center the video */
-        z-index: 0; /* Ensure video is behind the text */
+        min-width: 100%;
+        min-height: 100%;
+        width: auto;
+        height: auto;
+        transform: translate(-50%, -50%);
+        z-index: 0;
     }
 </style>
 
 <div class="carousel-element">
-    <video autoplay muted loop>
-        <source src={videoUrl} type="video/mp4" />
+    <video autoplay loop muted>
+        <source src={videoUrl} type="video/mp4"/>
         Your browser does not support the video tag.
     </video>
-    <div class="carousel-text">{text}</div>
+    {#if visible}
+        <div class="carousel-text" transition:scale="{{axis: 'y', delay: 400, duration: 1000}}">{text}</div>
+    {/if}
 </div>
